a98281476426954c8328303f6a5b20366fa4469a
FX 2301 <realfx2301@gmail.com>
2021-11-24 05:17:47 -0800

    Wire up curl2min skeleton.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from optparse import OptionParser
+from optparse import OptionGroup, OptionParser
 import sys
 
-from wrapper import execute
+from transformations.curl2min import minimal_curl_args
 
 parser = OptionParser(usage="usage: %prog [options] curl [curl_options]")
+
+transformations_group = OptionGroup(parser, "Transformations")
+
+transformations_group.add_option(
+    "--minimal-curl", "--curl", "--min",
+    action="store_true", dest="minimal_curl",
+    help="Remove all the curl options that have no effect on the response.")
+
+parser.add_option_group(transformations_group)
 
 try:
     curl_start = sys.argv.index('curl')
@@ -15,7 +24,20 @@ except ValueError:
 main_args = sys.argv[1:curl_start]
 curl_args = sys.argv[curl_start+1:]
 
-print('main_args', main_args)
-print('curl_args', curl_args)
+(options, remainder_args) = parser.parse_args(args=main_args)
 
-print(execute(curl_args))
+if len(remainder_args) > 0:
+    print(f"Unexpected argument(s) before curl statement: {' '.join(remainder_args)}", file=sys.stderr)
+    parser.print_help(file=sys.stderr)
+    exit(1)
+
+if not(options.minimal_curl):
+    print(f"Expected at least one transformation option.", file=sys.stderr)
+    parser.print_help(file=sys.stderr)
+    exit(1)
+
+if options.minimal_curl:
+    curl_args = minimal_curl_args(curl_args)
+
+# TODO quote command for safe usage in bash e.g. -H 'Host: foo'
+print(' '.join(['curl']+curl_args))
---
+++ transformations/curl2min.py
@@ -0,0 +1,5 @@
+from typing import List
+from wrapper import execute
+
+def minimal_curl_args(curl_args: List[str]) -> List[str]:
+    return curl_args
