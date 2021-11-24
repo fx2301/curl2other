2d399ae14f10f4421372fc47e7737022bf113285
FX 2301 <realfx2301@gmail.com>
2021-11-24 08:06:21 -0800

    Identify semantic curl options. Start on minimal curl implementation.

--- transformations/curl2min.py
+++ transformations/curl2min.py
@@ -1,5 +1,48 @@
+import os
+import sys
+from functools import reduce
 from typing import List
-from wrapper import execute
 
-def minimal_curl_args(curl_args: List[str]) -> List[str]:
+from wrapper import execute_options, to_options
+
+IGNORED_OPTIONS = ['-v', '--verbose', '-s', '--silent']
+
+def _fail(msg):
+    sys.stderr.write('Aborting. ')
+    sys.stderr.write(msg)
+    sys.stderr.write(' See https://github.com/fx2301/curl2min#Troubleshooting\n')
+    exit(1)
+
+def minimal_curl_args(curl_args: List[str], verbose: bool = True) -> List[str]:
+    log = sys.stderr if verbose else open(os.devnull, 'w')
+
+    # set aside ignored options
+    options_required = [
+        option
+        for option in to_options(curl_args)
+        if option.name in IGNORED_OPTIONS
+    ]
+    options_undecided = [
+        option
+        for option in to_options(curl_args)
+        if option.name not in IGNORED_OPTIONS
+    ]
+    
+    log.write("Testing for identical responses...\n")
+    result_initial = execute_options(options_required + options_undecided)
+    result = execute_options(options_required + options_undecided)
+    if result_initial != result:
+        _fail(f"Expected identical responses for identical calls: {result_initial} vs {result}")
+
+    # expand options into finer grained sets of options
+    options_undecided = reduce(
+        lambda acc, option: acc + option.to_component_options(),
+        options_undecided,
+        []
+    )
+    log.write("Testing for identical response after expanding options into finer grained options...\n")
+    result = execute_options(options_required + options_undecided)
+    if result_initial != result:
+        _fail(f"Report this bug! Expected identical responses: {result_initial} vs {result}")
+
     return curl_args
--- wrapper.py
+++ wrapper.py
@@ -1,8 +1,10 @@
 from dataclasses import dataclass
 import hashlib
+import json
 import re
 import subprocess
-from typing import List
+from typing import Dict, List, Optional
+from functools import reduce
 
 @dataclass
 class CurlResult:
@@ -14,7 +16,7 @@ class CurlResult:
     def __str__(self):
         return f"CurlResult(status={self.status}, body_digest={self.body_digest})"
 
-def execute(arguments: List[str]) -> (str, str):
+def execute(arguments: List[str]) -> CurlResult:
     cmd = ['curl', '-v', '-s'] + arguments
     if '-v' not in cmd:
         cmd.append('-v')
@@ -37,3 +39,61 @@ def execute(arguments: List[str]) -> (str, str):
     # print(status, digest)
 
     return CurlResult(arguments, status, body_digest)
+
+# TODO memoize this?
+def _option_definitions() -> Dict[str, dict]:
+    with open('curl_options.json', 'r') as f:
+        options = json.load(f)
+        option_definitions = {}
+        for option in options:
+            option_definitions[option['option']] = option
+            if option['alias'] is not None:
+                option_definitions[option['alias']] = option
+    
+    return option_definitions
+
+@dataclass
+class CurlOption:
+    """Semantic option for curl command-line"""
+    name: str
+    argument: Optional[str] = None
+
+    def to_arguments(self):
+        if self.argument is None:
+            return [self.name]
+        else:
+            return [self.name, self.argument]
+
+    def to_component_options(self):
+        return [self]
+
+def to_options(arguments: List[str]) -> List[CurlOption]:
+    option_definitions = _option_definitions()
+
+    options = []
+    i = 0
+    while i < len(arguments):
+        option_name = arguments[i]
+        i += 1
+
+        if option_name not in option_definitions:
+            if option_name.startswith("-"):
+                print(f"WARNING: unrecognized curl option: {option_name}")
+            options.append(CurlOption(name=option_name))
+        else:
+            if option_definitions[option_name]['argument'] is None:
+                options.append(CurlOption(name=option_name))
+            else:
+                if i < len(arguments):
+                    options.append(CurlOption(name=option_name, argument=arguments[i]))
+                    i += 1
+                else:
+                    raise Exception(f"Missing argument for curl option {option_name}")
+
+    return options
+
+def execute_options(options: List[CurlOption]) -> (str, str):
+    arguments = reduce(lambda acc, option: acc + option.to_arguments(), options, [])
+
+    return execute(arguments)
+
